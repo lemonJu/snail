b76841fc68cb22bf2cfb7a37d546b7b6b1ea2252
lixinqi <250626072@qq.com>
2015-06-08 21:15:18 +0800

    lxq

--- routes/log.js
+++ routes/log.js
@@ -5,15 +5,7 @@ module.exports = {
     login : function (req, res, next){
         var username = req.body.username;
         var userage = req.body.userage;
-<<<<<<< HEAD
-        //console.log(username + userage);
         res.send("login你好");
-        //res.redirect("/loginOk.html");
-=======
-        console.log(username + userage);
-        //res.send("login:" + username +"<br/>userage:" + userage);
-        res.redirect("/loginOk.html");
->>>>>>> c63dad16969a7a305a219bea0fcaba8f44135dbd
     },
     logup : function (req, res, next){
         res.send("log-logup");
